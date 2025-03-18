0a9c108318a933c0b5141c9b563c5168f418bceb
kraison <oun.cav@gmail.com>
2025-03-18 17:09:58 +0700

    20241803

---
+++ components/Home/Navbar/MobileNav.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { navLinks } from "@/constant/Constant";
+import Link from "next/link";
+import { CgClose } from "react-icons/cg";
+type Props = {
+  showNav: boolean;
+  closeNav: () => void;
+};
+
+const MobileNav = ({ closeNav, showNav }: Props) => {
+  const navOpen = showNav ? "translate-x-0" : "translate-x-[-100%]";
+
+  return (
+    <div className="overflow-hidden h-[2000px]">
+      {/* overlay  */}
+      <div
+        className={`fixed ${navOpen} inset-0 transform transition-all duration-200 z-[10000] bg-black opacity-75 h-screen w-full`}
+      >
+        {/* Navlinks  */}
+        <div className="text-white fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-indigo-900 space-y-6 z-[10006]">
+          {navLinks.map((link) => {
+            return (
+              <Link href={link.url} key={link.id}>
+                <p className="nav__link text-white text-[20px] ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]">
+                  {link.label}
+                </p>
+              </Link>
+            );
+          })}
+          {/* Close icon  */}
+          <CgClose
+            onClick={closeNav}
+            className="absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileNav;
---
+++ components/Home/Navbar/Nav.tsx
@@ -0,0 +1,63 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { navLinks } from "@/constant/Constant";
+import { HiBars3BottomRight } from "react-icons/hi2";
+import { useEffect, useState } from "react";
+
+type Props = {
+  openNav: () => void;
+};
+
+const Nav = ({ openNav }: Props) => {
+  const [navBg, setNavBg] = useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 90) setNavBg(true);
+      if (window.scrollY < 90) setNavBg(false);
+    };
+    window.addEventListener("scroll", handler);
+    return () => {
+      window.removeEventListener("scroll", handler);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`w-full ${
+        navBg ? "bg-white shadow-md" : "fixed"
+      } transition-all duration-200 h-[12vh] z-[1000]`}
+    >
+      <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
+        {/* Logo  */}
+        <h1 className="text-xl md:text-2xl font-bold">
+          <span className="text-3xl md:text-4xl text-pink-700">A</span>ppify
+        </h1>
+        {/* Nav Links */}
+        <nav className="hidden lg:flex items-center space-x-10">
+          {navLinks.map((link) => {
+            return (
+              <Link href={link.url} key={link.id}>
+                <p className="nav__link">{link.label}</p>
+              </Link>
+            );
+          })}
+        </nav>
+        {/* Button  */}
+        <div className="flex items-center space-x-4">
+          <button className="md:px-8 md:py-2.5 px-6 py-2 text-white font-semibold text-base bg-blue-700 hover:bg-blue-900 transition-all duration-200 rounded-full">
+            Join Now
+          </button>
+          {/* Burger menu  */}
+          <HiBars3BottomRight
+            onClick={openNav}
+            className="w-8 h-8 cursor-pointer text-black lg:hidden"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;

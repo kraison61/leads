00f68f1b6a69cb27d2f768ce2f7615c13db58e75
kraison <oun.cav@gmail.com>
2025-03-27 16:28:39 +0700

    2025-03-27

--- app/content/[id]/page.tsx
+++ app/content/[id]/page.tsx
@@ -1,6 +1,23 @@
-const pageContent = () => {
-  return (
-    <div>pageContent</div>
-  )
+import { contentData } from "@/constant/Constant";
+
+//Generate static paths at build time
+export function generateStaticParams() {
+  return contentData.map((item) => ({
+    id: item.id,
+  }));
 }
-export default pageContent
+
+//Create SEO-friendly metadata
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const { id } = await params;
+  const item = contentData.find((item) => item.id === id);
+
+  return {
+    title: item.title.split("|")[0].trim(),
+  };
+}
+
+const pageContent = () => {
+  return <div>pageContent</div>;
+};
+export default pageContent;
